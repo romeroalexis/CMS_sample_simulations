364b68e6daef87324fdafedbf80cef600c2a4d47
Alexis Romero <romeroa@mit.edu>
2015-08-10 12:43:06 -0700

    Update Fastjet_main.cc

--- Fastjet_main.cc
+++ Fastjet_main.cc
@@ -8,10 +8,21 @@
 using namespace fastjet;
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
     
-    ifstream myreadfile_4m("pythia_four_momentum.dat");     // Read particles' four-momenta from file
-    ifstream myreadfile_weight("pythia_weight.dat");       // Read bins' weights from file
+    //This program reads command line arguments:
+    //Argv[0]: filename, Argv[1]: pTmin, Argv[2]: Zcut
+    //End program if the no. of arguments provided is not 3
+    if (argc != 3) {
+        cout << "Run with: ./fastjet ptmin z_cut" << endl;
+        cout << "    e.g.: ./fastjet 150. 0.05" << endl;
+        return 0;
+    }
+    
+    ofstream mywritefile;
+    
+    ifstream myreadfile_4m("pythia_four_momentum.dat");     //Read four-momentum from file
+    ifstream myreadfile_weight("pythia_weights.dat");       //Read bin weight from file
     
     string line;
     
@@ -20,17 +31,32 @@ int main() {
     while (getline (myreadfile_weight,line)) {
         
         istringstream linestream(line);
-        linestream >> weight[iBin];         // store weights in string
-        iBin++;
+        linestream >> weight[iBin];                         //Store weights in a string
+        iBin++;                                             //Advance bin no.
     }
     
+    double ptmin = atof(argv[1]);                           //Read pTmin from command line argument
+    double z_cut = atof(argv[2]);                           //Read Zcut from command line argument
+    int Z_cut_file = (int) (z_cut*100);                     //Integer part of (Zcut*100)
+    
+    //Output file is named after the Zcut.
+    //E.g.: "Zcut_5.dat" for a Zcut or 0.05
+    mywritefile.open("Zcut_" + to_string(Z_cut_file) + ".dat");
+    
+    // mywritefile header
+    //mywritefile << "---------------------------------------------------------------------------" << endl;
+    //mywritefile << "                 Symmetry" << "      Transverse Momentum" << "                   Weight" << endl;
+    //mywritefile << "                     (Zg)" << "                     (pT)" << endl;
+    //mywritefile << "---------------------------------------------------------------------------" << endl;
+    
+    // Do while iEvent < bins*100000
     int iEvent = 0;
     while (iEvent < 1300000) {
         
         vector<PseudoJet> input_particles;
         
-        int Bin, Event;
-        double Px, Py, Pz, E;
+        int Bin, Event;                                     //Bin and Event
+        double Px, Py, Pz, E;                               //Four-momentum components
         
         while (getline (myreadfile_4m,line)) {
             
@@ -41,17 +67,16 @@ int main() {
             }
             
             if (line.substr(0,4) != "#END")
-                linestream >> Bin >> Px >> Py >> Pz >> E;
+                linestream >> Bin >> Event >> Px >> Py >> Pz >> E;
             input_particles.push_back(PseudoJet(Px, Py, Pz, E));
         }
         
-        double R = 0.5;         // Jet radius
-        double ptmin = 150.0;   // Define min transverse momentum
-        double z_cut = 0.05;    // Define symmetry cut
-        double beta = 0.0;
+        double R = 0.5;                                     //Jet radius
+        double beta = 0.0;                                  //Soft Drop parameter
         
         contrib:: SoftDrop sd(beta,z_cut);
         
+        //Jet algorithm use: antikt
         JetDefinition jet_def(fastjet::antikt_algorithm, R);
         ClusterSequence clust_seq(input_particles, jet_def);
         vector<PseudoJet> inclusive_jets = sorted_by_pt(clust_seq.inclusive_jets(ptmin));
@@ -61,11 +86,12 @@ int main() {
             PseudoJet sd_jet = sd(inclusive_jets[i]);
             
             if (i == 0) {
-                
-                // Write symmetry (zg), pT, and weight to file
-                mywritefile << setw(15) << sd_jet.structure_of<contrib::SoftDrop>().symmetry() << setw(15) << inclusive_jets[i].perp() << setw(15) << weight[Bin] << endl;
-                //cout << Bin << "   Symmetry= " << sd_jet.structure_of<contrib::SoftDrop>().symmetry() << "   pT=" << inclusive_jets[i].perp() << "   weight=  " << weight[Bin] << endl;
+                //Output the hardest jet to file
+                mywritefile << setw(25) << sd_jet.structure_of<contrib::SoftDrop>().symmetry() << setw(25) << inclusive_jets[i].perp() << setw(25) << weight[Bin] << endl;
+                cout << "bin=  " << Bin << "   Event = " << iEvent << endl;
             }
         }
     }
+    mywritefile.close();
+    return 0;
 }
